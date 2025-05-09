dd0c7437ebeb2337fc3d05a8b0d0a4e94c77f49b
Shubham Kolar <shubhamkolar12@gmail.com>
2025-05-09 19:31:31 +0530

    Added important points related to TreeMap, NavigableMap, SortedMap

---
+++ Collections/HashMaps.java
@@ -0,0 +1,44 @@
+package Collections;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class HashMaps {
+    public static void main(String[] args) {
+        
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Shubham", 1);
+        map.put("Tejam", 2);
+        map.put("Ravi", 3);
+        map.put("Ravi", 3); // Will be ignored since its duplicate
+        map.put("Rohit", 4);
+    
+        System.out.println(map);
+
+        map.remove("Rohit");
+        System.out.println(map.getOrDefault("Rohit", 0)); // This is used to avoid any exceptions if the value don't exists
+        System.out.println(map.getOrDefault("Shubham", 1));
+
+        System.out.println(map.containsKey("Tejam"));
+        System.out.println("--------------------------------------------------------");
+
+// The entrySet() method of the Map interface returns a Set of key-value pairs. It's commonly 
+// used when you want to iterate over both keys and values at the same time.
+
+        Set<Map.Entry<String, Integer>> entrySet = map.entrySet();
+        for(Map.Entry<String, Integer> entry : entrySet)
+            System.out.println(entry.getKey() + ", " + entry.getValue());
+
+// The keySet() methods returns a Set of all keys in the map. It allows you to iterate over the keys and
+// across values using get(key).
+        Set<String> keySets = map.keySet();
+        for(String key : keySets){
+            System.out.println(key + ", " + map.get(key)); // need to call map.get(key) for values to get
+        }
+
+// LinkedHashMap is just get the ordering elements and implement the same methods. 
+
+    }
+    
+}
