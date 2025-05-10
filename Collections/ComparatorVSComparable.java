13884fb7db9724ebb018cd55c3f303490bb70229
Shubham Kolar <shubhamkolar12@gmail.com>
2025-05-10 07:38:02 +0530

    updated the ComparatorVSComparable file

--- Collections/ComparatorVSComparable.java
+++ Collections/ComparatorVSComparable.java
@@ -5,8 +5,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-// public class ComparatorVSComparable {
-
 class Student implements Comparable<Student> {
     int rollNo;
     String name;
